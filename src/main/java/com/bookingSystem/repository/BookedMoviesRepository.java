8b885d799d68fb05e5309b30b5fef427ddcd47b7
Mohamed Mostafa <91856839+muhammed-elsherif@users.noreply.github.com>
2024-05-07 19:23:30 +0300

    Delete src directory

--- src/main/java/com/bookingSystem/repository/BookedMoviesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookingSystem.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.bookingSystem.entity.BookedMovies;
-
-@Repository
-public interface BookedMoviesRepository extends JpaRepository<BookedMovies, Integer> {
-    
-}
